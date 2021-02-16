b35f2cc6335650dbbb814917e840ab8d9d8d480b
Marek Fajkus <marek.faj@gmail.com>
2021-02-17 00:52:12 +0100

    add conditions to cabal file

--- src/System/Directory/Watch/Linux.hs
+++ src/System/Directory/Watch/Linux.hs
@@ -45,10 +45,12 @@ type Id = Inotify.Watch
 
 initBackend :: IO Handle
 initBackend = Inotify.init
+{-# INLINE initBackend #-}
 
 
 closeBackend :: Handle -> IO ()
 closeBackend = Inotify.close
+{-# INLINE closeBackend #-}
 
 
 toEvent :: FilePath -> Inotify.Event -> Event
@@ -58,31 +60,38 @@ toEvent path Inotify.Event{..} = Event{..}
     eventType
         | Inotify.isSubset Inotify.in_ISDIR mask = MkDir
         | otherwise = Touch
+{-# INLINE toEvent #-}
 
 
 addTouch :: Handle -> FilePath -> IO Inotify.Watch
 addTouch handle path =
     Inotify.addWatch handle path Inotify.in_MODIFY
+{-# INLINE addTouch #-}
 
 
 addMkDir :: Handle -> FilePath -> IO Inotify.Watch
 addMkDir handle path =
     Inotify.addWatch handle path Inotify.in_CREATE
+{-# INLINE addMkDir #-}
 
 
 getBackendEvent :: Handle -> IO BackendEvent
 getBackendEvent = Inotify.getEvent
+{-# INLINE getBackendEvent #-}
 
 
 isDirectory :: BackendEvent -> Bool
 isDirectory Inotify.Event{..} =
     Inotify.isSubset Inotify.in_ISDIR mask
+{-# INLINE isDirectory #-}
 
 
 getId :: BackendEvent -> Id
 getId Inotify.Event{..} = wd
+{-# INLINE getId #-}
 
 
 internalWatch :: Handle -> FilePath -> IO Id
 internalWatch handle path =
     Inotify.addWatch handle path Inotify.in_DELETE_SELF
+{-# INLINE internalWatch #-}
